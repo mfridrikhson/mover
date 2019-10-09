bf7500b6613ad67c530489d054789c0daede74f7
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-10-10 00:47:45 +0300

    feat: Added registration form view

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from '../../scenes/Homepage';
+import RegistrationPage from '../../scenes/RegistrationPage';
 import NotFound from '../../scenes/NotFound';
 
 const Routing = () => {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
+      <Route exact path="/register" component={RegistrationPage} />
       <Route exact path="*" component={NotFound} />
     </Switch>
   );
---
+++ client/src/scenes/RegistrationPage/index.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { Button, Form } from 'semantic-ui-react';
+
+import CenteringContainer from '../../components/CenteringContainer';
+
+import styles from './styles.module.scss';
+
+const validationSchema = yup.object().shape({
+  email: yup.string()
+    .email('Invalid email address')
+    .required('Email address is required'),
+  password: yup.string()
+    .min(8, 'Minimum length - 8 characters')
+    .max(72)
+    .matches(
+      /^(?=.*)(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d].{8,}$/,
+      'Password must include at least one number and one letter'
+    )
+    .required('Password is required'),
+  firstName: yup.string().max(250),
+  lastName: yup.string().max(250)
+});
+
+class RegistrationPage extends React.Component {
+  onFormSubmit = (values, { setSubmitting }) => {
+    setSubmitting(true);
+  };
+
+  render() {
+    return (
+      <CenteringContainer>
+        <div className={styles.registerFormContainer}>
+          <h1>Sign Up</h1>
+          <Formik
+            initialValues={{
+              email: '',
+              password: '',
+              isDriver: false,
+              firstName: '',
+              lastName: '',
+            }}
+            onSubmit={this.onFormSubmit}
+            validationSchema={validationSchema}
+          >
+            {({ values, errors, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+              <Form onSubmit={handleSubmit}>
+                <Form.Input
+                  value={values.email}
+                  error={errors.email}
+                  label="E-mail"
+                  placeholder="E-mail address"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  disabled={isSubmitting}
+                  required
+                />
+                <Form.Input
+                  value={values.password}
+                  error={errors.password}
+                  label="Password"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  disabled={isSubmitting}
+                  required
+                />
+                <Form.Checkbox
+                  value={values.isDriver}
+                  label="Register as a driver"
+                  name="isDriver"
+                  onChange={handleChange}
+                  disabled={isSubmitting}
+                  slider
+                />
+                <Form.Group widths="equal">
+                  <Form.Input
+                    value={values.firstName}
+                    error={errors.firstName}
+                    label="First Name"
+                    placeholder="First Name"
+                    type="text"
+                    name="firstName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    disabled={isSubmitting}
+                  />
+                  <Form.Input
+                    value={values.lastName}
+                    error={errors.lastName}
+                    label="Last Name"
+                    placeholder="Last Name"
+                    type="text"
+                    name="lastName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    disabled={isSubmitting}
+                  />
+                </Form.Group>
+                <Button primary fluid type="submit" loading={isSubmitting}>Sign Up</Button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </CenteringContainer>
+    );
+  }
+}
+
+export default RegistrationPage;
