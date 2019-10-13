ced217bd7ad657caa1622e839f4fe2c20315613b
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-10-14 00:09:26 +0300

    feat: Added Mapbox GL component

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from '../../scenes/Homepage';
 import RegistrationPage from '../../scenes/RegistrationPage';
 import LoginPage from '../../scenes/LoginPage';
+import Moving from '../../scenes/Moving';
 import NotFound from '../../scenes/NotFound';
 
 const Routing = () => {
@@ -11,6 +12,7 @@ const Routing = () => {
       <Route exact path="/" component={Homepage} />
       <Route exact path="/register" component={RegistrationPage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/moving" component={Moving} />
       <Route exact path="*" component={NotFound} />
     </Switch>
   );
---
+++ client/src/scenes/Moving/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from '../../containers/Map';
+
+class Moving extends React.Component {
+  render() {
+    return <Map/>;
+  }
+}
+
+export default Moving;
