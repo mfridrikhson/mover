271707f0bc429dcae41e1295483f18e0872b6312
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-11-20 22:05:17 +0200

    Fixed bucket name

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,7 +4,7 @@ npm run build
 rm -rf !("build")
 cd ..
 pip install --user awscli
-aws s3 cp s3://depotbucket/.env.stage server/.env
+aws s3 cp s3://movers3/.env.stage server/.env
 zip -r latest * > /dev/null
 mkdir -p dpl_cd_upload
 mv latest.zip dpl_cd_upload/latest.zip
