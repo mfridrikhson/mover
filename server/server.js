c2f146dd4dbcb732bd0695efdcea3b1f9e68962f
Mamontenokk <vash1@ukr.net>
2019-11-21 00:20:55 +0200

    feat: Code refactor

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const serve = require('koa-static');
-const mount = require('koa-mount');
+//const mount = require('koa-mount');
 const passport = require('koa-passport');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -28,11 +28,12 @@ app.use(socketInjector(io));
 
 setupRoutesForApp(app);
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
   const staticPages = new Koa();
   staticPages.use(serve(__dirname + '../client/build')); //serve the build directory
   app.use(mount('/', staticPages));
-}
+}*/
+app.use(serve(__dirname + '../client/build'));
 
 const server = app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
