78421179f14fea1e064267907a8e968792ee80e5
Mamontenokk <vash1@ukr.net>
2019-11-22 20:16:25 +0200

    feat: added env for client

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+pip install --user awscli
+aws s3 cp s3://movers3/.env.stage server/.env
+aws s3 cp s3://movers3/.env.client client/.env
 cd client/
 npm run build
 rm -rf !("build")
 cd ..
-pip install --user awscli
-aws s3 cp s3://movers3/.env.stage server/.env
 zip -r latest * > /dev/null
 mkdir -p dpl_cd_upload
 mv latest.zip dpl_cd_upload/latest.zip
