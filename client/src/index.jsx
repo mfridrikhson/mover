18a3dfacd5d0782aaeb090b3c1d41cab3365e7ad
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-11-23 20:54:15 +0200

    fix: Fixed styles in built version

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scenes/App';
 
-import './styles/reset.scss';
-import './styles/common.scss';
 import 'semantic-ui-css/semantic.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import './styles/common.scss';
 
 ReactDOM.render(
   <App />,
