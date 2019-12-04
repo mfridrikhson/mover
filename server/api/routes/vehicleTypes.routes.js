e80ad24ec45f18f5205fee7e24898346a872bd9b
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-12-04 23:51:00 +0200

    test: Added tests for /api/vehicle-types

--- server/api/routes/vehicleTypes.routes.js
+++ server/api/routes/vehicleTypes.routes.js
@@ -1,35 +1,10 @@
 const Router = require('@koa/router');
 const router = new Router({ prefix: '/vehicle-types' });
 
-const {
-  getAllVehicleTypes,
-  getVehicleTypeById,
-  addVehicleType,
-  deleteVehicleTypeById,
-  updateVehicleTypeById
-} = require('../services/vehicleTypes.service');
+const { getAllVehicleTypes } = require('../services/vehicleTypes.service');
 
 router.get('/', async (ctx) => {
   ctx.body = await getAllVehicleTypes();
-});
-
-router.get('/:id', async (ctx) => {
-  ctx.body = await getVehicleTypeById(ctx.params.id);
-});
-
-router.post('/', async (ctx) => {
-  const [vehicleType] = await addVehicleType(ctx.request.body);
-  ctx.body = vehicleType;
-});
-
-router.delete('/:id', async (ctx) => {
-  const [vehicleType] = await deleteVehicleTypeById(ctx.params.id);
-  ctx.body = vehicleType;
-});
-
-router.put('/:id', async (ctx) => {
-  const [vehicleType] = await updateVehicleTypeById(ctx.params.id, ctx.request.body);
-  ctx.body = vehicleType;
 });
 
 module.exports = router;
--- server/api/services/vehicleTypes.service.js
+++ server/api/services/vehicleTypes.service.js
@@ -1,25 +1,5 @@
-const {
-  getAll,
-  getById,
-  add,
-  deleteById,
-  updateById
-} = require('../../data/queries/vehicleTypes.query');
+const { getAll } = require('../../data/queries/vehicleTypes.query');
 
 const getAllVehicleTypes = () => getAll();
 
-const getVehicleTypeById = (id) => getById(id);
-
-const addVehicleType = (vehicleType) => add(vehicleType);
-
-const deleteVehicleTypeById = (id) => deleteById(id);
-
-const updateVehicleTypeById = (id, vehicleType) => updateById(id, vehicleType);
-
-module.exports = {
-  getAllVehicleTypes,
-  getVehicleTypeById,
-  addVehicleType,
-  deleteVehicleTypeById,
-  updateVehicleTypeById
-};
+module.exports = { getAllVehicleTypes };
