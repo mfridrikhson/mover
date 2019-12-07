27bab60a61d0c24b82bf2a8d146659e603b896b8
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-12-07 14:03:55 +0200

    tests: Added tests for POST

--- server/test/routes.vehicles.test.js
+++ server/test/routes.vehicles.test.js
@@ -13,12 +13,48 @@ describe('routes : vehicles', () => {
         password: 'test123456',
         firstName: 'Bob',
         lastName: 'Snow',
-        isDriver: false
+        isDriver: true
       });
   });
 
   afterAll(() => {
     return knex.destroy();
+  });
+
+  describe('POST /api/vehicles', () => {
+    it('should return added vehicle', async (done) => {
+      const resWithToken = await request
+        .post('/api/auth/login')
+        .send({
+          email: 'test@gmail.com',
+          password: 'test123456'
+        });
+
+      const { body: vehicleTypes } = await request
+        .get('/api/vehicle-types');
+
+      request
+        .post('/api/vehicles')
+        .set('Authorization', `Bearer ${ resWithToken.body.token }`)
+        .send({
+          name: 'Daewoo Lanos',
+          registrationPlate: 'АА 12345 АН',
+          color: '#000000',
+          vehicleTypeId: vehicleTypes[0].id,
+          photoUrl: 'http://some.url',
+          techPassportUrl: 'http://some.url',
+          driverId: resWithToken.body.driver.id
+        })
+        .expect(201)
+        .end((err, res) => {
+          console.log(res.body);
+          expect(err).toBeNull();
+          /*expect(res.body).toEqual(
+
+          );*/
+          done();
+        });
+    });
   });
 
   describe('GET /api/vehicles', () => {
@@ -29,24 +65,6 @@ describe('routes : vehicles', () => {
           email: 'test@gmail.com',
           password: 'test123456'
         });
-      const expectedData = [
-        {
-          "type":"Minivan",
-          "pricePerKm": 2.5
-        },
-        {
-          "type":"Van",
-          "pricePerKm": 5
-        },
-        {
-          "type":"3-Ton truck",
-          "pricePerKm": 10
-        },
-        {
-          "type":"5-Ton truck",
-          "pricePerKm": 15
-        }
-      ];
 
       request
         .get('/api/vehicle-types')
