9e2d76ae72de8b5a570c3020b8c463bb814bcf6d
Mamontenokk <vash1@ukr.net>
2019-12-08 14:38:52 +0200

    feat: added tests for auth services

--- server/helpers/crypto.helper.js
+++ server/helpers/crypto.helper.js
@@ -4,6 +4,5 @@ const saltRounds = 10;
 
 module.exports = {
   encrypt: (data) => bcrypt.hash(data, saltRounds),
-  encryptSync: data => bcrypt.hashSync(data, saltRounds),
   compare: (data, encrypted) => bcrypt.compare(data, encrypted)
 };
